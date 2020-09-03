fcc235cadccf9ec5575643d38d345bc023efb029
Natalia Beckstead <toosmiravilla@gmail.com>
2020-09-03 15:15:18 -0600

    added basic layout and stylings

--- src/App.js
+++ src/App.js
@@ -5,7 +5,27 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <p>Hi</p>
+      <div className="calculator">
+        <p>0</p>
+        <div className="calc-buttons">
+          <button className="calc-button is-clear">C</button>
+          <button className="calc-button">&divide;</button>
+          <button className="calc-button">7</button>
+          <button className="calc-button">8</button>
+          <button className="calc-button">9</button>
+          <button className="calc-button">&times;</button>
+          <button className="calc-button">4</button>
+          <button className="calc-button">5</button>
+          <button className="calc-button">6</button>
+          <button className="calc-button">&minus;</button>
+          <button className="calc-button">1</button>
+          <button className="calc-button">2</button>
+          <button className="calc-button">3</button>
+          <button className="calc-button">+</button>
+          <button className="calc-button is-zero">0</button>
+          <button className="calc-button is-equals">=</button>
+        </div>
+      </div>
     </div>
   );
 }
