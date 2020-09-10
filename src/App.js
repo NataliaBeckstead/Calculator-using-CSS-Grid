1dfb7472d41c71ce1ba78d4e3ab518786e435726
Natalia Beckstead <toosmiravilla@gmail.com>
2020-09-09 22:09:40 -0600

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.scss';
+import * as math from 'mathjs';
 
 function App() {
+
+  const[result, setResult] = useState("");
+
+  function handleClear () {
+    setResult("");
+  }
+
+  function addInput(str) {
+    setResult(result + str);
+  }
+
+  function handleEqual() {
+    setResult(math.evaluate(result));
+  }
+
   return (
     <div className="App">
       <div className="calculator">
-        <p>0</p>
+        <p>{result}</p>
         <div className="calc-buttons">
-          <button className="calc-button is-clear">C</button>
-          <button className="calc-button">&divide;</button>
-          <button className="calc-button">7</button>
-          <button className="calc-button">8</button>
-          <button className="calc-button">9</button>
-          <button className="calc-button">&times;</button>
-          <button className="calc-button">4</button>
-          <button className="calc-button">5</button>
-          <button className="calc-button">6</button>
-          <button className="calc-button">&minus;</button>
-          <button className="calc-button">1</button>
-          <button className="calc-button">2</button>
-          <button className="calc-button">3</button>
-          <button className="calc-button">+</button>
-          <button className="calc-button is-zero">0</button>
-          <button className="calc-button is-equals">=</button>
+          <button className="calc-button is-clear" onClick = {handleClear}>C</button>
+          <button className="calc-button" onClick = {() => addInput("%")}>&divide;</button>
+          <button className="calc-button" onClick = {() => addInput("7")}>7</button>
+          <button className="calc-button" onClick = {() => addInput("8")}>8</button>
+          <button className="calc-button" onClick = {() => addInput("9")}>9</button>
+          <button className="calc-button" onClick = {() => addInput("*")}>&times;</button>
+          <button className="calc-button" onClick = {() => addInput("4")}>4</button>
+          <button className="calc-button" onClick = {() => addInput("5")}>5</button>
+          <button className="calc-button" onClick = {() => addInput("6")}>6</button>
+          <button className="calc-button" onClick = {() => addInput("-")}>&minus;</button>
+          <button className="calc-button" onClick = {() => addInput("1")}>1</button>
+          <button className="calc-button" onClick = {() => addInput("2")}>2</button>
+          <button className="calc-button" onClick = {() => addInput("3")}>3</button>
+          <button className="calc-button" onClick = {() => addInput("+")}>+</button>
+          <button className="calc-button is-zero" onClick = {() => addInput("0")}>0</button>
+          <button className="calc-button" onClick = {() => addInput(".")}>.</button>
+          <button className="calc-button is-equals" onClick = {handleEqual}>=</button>
         </div>
       </div>
     </div>
